ef09e11ab5d8ee1bf443d2cce44b903d6d38fac6
Abhijit-singh04 <103450664+Abhijit-singh04@users.noreply.github.com>
2022-05-30 00:11:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ const bodyParser = require('body-parser')
 
 
 // SET OUR VIEWS AND VIEW ENGINE
-app.use('/public', express.static(path.join(_dirname,'public')))
-app.use('/models', express.static(path.join(_dirname ,'models')))
-app.use('/static', express.static(path.join(_dirname , 'static')))
+app.use('/public', express.static(path.join(__dirname,'public')))
+app.use('/models', express.static(path.join(__dirname ,'models')))
+app.use('/static', express.static(path.join(__dirname , 'static')))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json({
